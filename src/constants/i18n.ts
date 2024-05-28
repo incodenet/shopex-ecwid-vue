e8f098e81953bb562680771515166ab5a3d83a26
Erik <erik.petri@yandex.ru>
2024-05-28 10:53:14 +0400

    hotfix: import enums in i18n

--- src/constants/i18n.ts
+++ src/constants/i18n.ts
@@ -1,6 +1,6 @@
 import { createI18n } from 'vue-i18n'
 import { messages } from './translations'
-import { LocalesEnum } from '@/enums'
+import { LocalesEnum } from '../enums'
 
 export const i18n = createI18n({
   globalInjection: true,
